cfdfab8c9d64f9e02ed2fc3b20b49380b5148d2d
manyi <714357249@qq.com>
2021-07-10 20:28:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author manyi
+ * @create 2021-07-10-20:24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
